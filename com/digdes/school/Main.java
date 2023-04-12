93b06bf60e14dde2680edca2304e992b96c21671
arsenijj <arsenijjnickitin2001@gmail.com>
2023-04-12 23:39:48 +0400

    deleted queries in main

--- com/digdes/school/Main.java
+++ com/digdes/school/Main.java
@@ -16,17 +16,6 @@ public class Main{
 
         // JavaSchoolStarter a = new JavaSchoolStarter();
 
-        JavaSchoolStarter a = new JavaSchoolStarter();
-  
-        List<Map<String, Object>> r1 = a.execute("INSERT VALUES 'lastName' = '1Федоров1', 'id' = 1, 'age' = 10, 'active' = true, 'cost' = 3.1;");
-        List<Map<String, Object>> r2 = a.execute("INSERT VALUES 'lastName' = 'Федоров2', 'id' = 2, 'age' = 42, 'active' = false, 'cost' = 3.2;");
-        List<Map<String, Object>> r3 = a.execute("INSERT VALUES 'lastName' = 'Федоров3', 'id' = 3, 'age' = 43, 'active' = true, 'cost' = 3.3;");
-        List<Map<String, Object>> r4 = a.execute("INSERT VALUES 'lastName' = 'Федоров4', 'id' = 4, 'age' = 44, 'active' = false, 'cost' = 3.4;");
-        List<Map<String, Object>> b = a.execute("select age, lastName, cost where active = 1");
-        List<Map<String, Object>> f = a.execute("select age, id where age = 1;");
-        List<Map<String, Object>> u= a.execute("UpDate active = true, lastName = 'Сеня' where id = 2 or cost >= 3.4");
-        List<Map<String, Object>> d= a.execute("delete where id = 1 or lastName ilike '%ФеДо%'");
-        System.out.println(table);
     }
  }
  
