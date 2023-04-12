938740e2d0d8a9c7fe594858afeb6188871fe3ac
arsenijj <arsenijjnickitin2001@gmail.com>
2023-04-13 01:59:51 +0400

    Added the ability of inserting a string with spaces into lastName

--- com/digdes/school/Insert.java
+++ com/digdes/school/Insert.java
@@ -1,4 +1,5 @@
 package com.digdes.school;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,9 +21,9 @@ public class Insert {
                     +
                     " ";
         }
-
-        query = left.split("\\s+");
-
+        // System.out.println(left);
+        query = left.split("[=] | ['id'] | ['lastName'] | ['cost'] | ['active'] | ['age']");
+        // System.out.println(Arrays.toString(query));
         Map<String, Object> row = new HashMap<>();
 
         row.put("id", null);
@@ -31,57 +32,54 @@ public class Insert {
         row.put("cost", null);
         row.put("active", null);
 
-        if (query.length < 3 || query.length % 3 != 0 || query.length > 15) {
+        if (query.length < 3 || query.length % 2 != 0 || query.length > 10) {
             throw new NullPointerException("Too few arguements");
 
         } else {
-            for (int i = 0; i < query.length - 2; i += 3) {
-                if (query[i + 1].equals("=")) {
-                    switch (query[i]) {
+            for (int i = 0; i < query.length - 1; i += 2) {
+                    switch (query[i].trim()) {
 
                         case "'lastName'", "lastName":
-                            row.put("lastName", query[i + 2]);
+                            row.put("lastName", query[i + 1]);
                             break;
-                        case "'id'", "id":
+                        case "id'", "id":
                             try {
                                 row.put("id",
-                                        Long.parseLong(query[i + 2]));
+                                        Long.parseLong(query[i + 1].trim()));
 
                             } catch (Exception e) {
                                 System.out.println("Value error in id.");
                             }
                             break;
-                        case "'active'", "active":
+                        case "active'", "active":
                             try {
 
                                 row.put("active",
-                                        Boolean.parseBoolean(query[i + 2]));
+                                        Boolean.parseBoolean(query[i + 1].trim()));
                             } catch (Exception e) {
                                 System.out.println("Value error in active.");
                             }
                             break;
-                        case "'age'", "age":
+                        case "age'", "age":
                             try {
 
                                 row.put("age",
-                                        Long.parseLong(query[i + 2]));
+                                        Long.parseLong(query[i + 1].trim()));
                             } catch (Exception e) {
                                 System.out.println("Value error in age.");
                             }
                             break;
-                        case "'cost'", "cost":
+                        case "cost'", "cost":
                             try {
 
                                 row.put("cost",
-                                        Double.parseDouble(query[i + 2]));
+                                        Double.parseDouble(query[i + 1].trim()));
                             } catch (Exception e) {
                                 System.out.println("Value error in cost.");
                             }
                             break;
+                        default : throw new NullPointerException("Arguement does not exist " + query[i]);
                     }
-                } else {
-                    return row;
-                }
             }
 
         }
